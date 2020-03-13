87e21a3131b9b009cc161e88f2b2d35a60092566
amogos <iulia.enes5@gmail.com>
2020-03-13 09:46:02 +0100

    write fragment test file with dummy content

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,50 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as fs from "fs";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "relay-snapshot-tests-generator" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "relay-snapshot-tests-generator" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "extension.createFragmentSnapshotTest",
+    e => {
+      vscode.window.showInputBox().then(filename => {
+        const path = `${e.fsPath}\\${filename}.js`;
+        const content = Buffer.from("some content\n");
+        writeContent(path, content);
+      });
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.createFragmentSnapshotTest', () => {
-		// The code you place here will be executed every time your command is executed
-		var content = 'empty';
-		var filename = vscode.window.activeTextEditor?.document.fileName;
-	///	var filePath = path.join(vscode.workspace.rootPath, selected.label + '.' + field);
-//fs.writeFileSync(filePath, content, 'utf8');
-		// Display a message box to the user
-		console.log(filename)
-		vscode.window.showInformationMessage("Fragment Snapshot Test Generated");
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
+
+const writeContent = (path: string, content: Buffer) => {
+  fs.open(path, "w", function(err: any, fd: any) {
+    if (err) {
+      vscode.window.showErrorMessage("error opening file: " + err);
+    }
+
+    fs.write(fd, content, 0, content.length, null, (err: any) => {
+      if (err) {
+        vscode.window.showErrorMessage("error writing file: " + err);
+      }
+      fs.close(fd, () =>
+        vscode.window.showInformationMessage("Test Template Generated")
+      );
+    });
+  });
+};
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
